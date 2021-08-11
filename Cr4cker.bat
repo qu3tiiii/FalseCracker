9e523520d6c7b8a353c19053ce31fbe05cb4ffe7
Biohazard <84819214+qu3tiiii@users.noreply.github.com>
2021-08-11 10:16:53 -0400

    Create Cr4cker.bat

---
+++ Cr4cker.bat
@@ -0,0 +1,32 @@
+@Echo off
+
+echo 1 Roblox Hacker
+echo 2 Discord Hacker
+echo 3 Fortnite Hacker
+set /p LoL= Elije:  
+if %LoL%==  (goto passss) else (goto passss)
+chcp 65001
+cls
+
+:passss
+color 4
+ 
+set /p Log= Usuario A Crackear:  
+if %Log%==  (goto casaxd) else (goto passss)
+chcp 65001
+cls
+
+
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
+msg * "TROLIAOO"
+msg * "Te fuiste rickroleaoooo"
+msg * "XDD"
+msg * "Never gonna give you up"
+msg * "XDD, xao"
+
